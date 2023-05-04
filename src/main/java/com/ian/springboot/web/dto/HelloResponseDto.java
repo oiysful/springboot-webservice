d1b381a0dc765b2b0086b472d7fc4fe3a1ee2329
이용상 <gaonoiys@gmail.com>
2023-05-04 18:04:48 +0900

    Chapter 4 End.

---
+++ src/main/java/com/ian/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.ian.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
