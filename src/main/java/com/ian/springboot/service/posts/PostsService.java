403b457986ea31fa9f743b642d0c27896a4e7033
이용상 <gaonoiys@gmail.com>
2023-05-05 22:04:31 +0900

    게시글 CRUD(등록, 목록조회, 수정, 삭제) 완료

--- src/main/java/com/ian/springboot/service/posts/PostsService.java
+++ src/main/java/com/ian/springboot/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.ian.springboot.service.posts;
 
 import com.ian.springboot.domain.posts.Posts;
 import com.ian.springboot.domain.posts.PostsRepository;
+import com.ian.springboot.web.dto.PostsListResponseDto;
 import com.ian.springboot.web.dto.PostsResponseDto;
 import com.ian.springboot.web.dto.PostsSaveRequestDto;
 import com.ian.springboot.web.dto.PostsUpdateRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,7 +29,6 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
@@ -35,5 +39,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAll(Sort.by(Sort.Direction.DESC, "id")).stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
