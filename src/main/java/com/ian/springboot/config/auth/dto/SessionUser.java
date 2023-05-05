449621a327f4d30218e357178293e9d1fb7c4202
이용상 <gaonoiys@gmail.com>
2023-05-05 23:11:36 +0900

    Spring Security, OAuth2.0 진행

---
+++ src/main/java/com/ian/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.ian.springboot.config.auth.dto;
+
+import com.ian.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
