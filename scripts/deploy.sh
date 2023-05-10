e3983f6472f4134984a7870fe87e27efa215a845
이용상 <gaonoiys@gmail.com>
2023-05-11 01:18:28 +0900

    codedeploy log test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,7 +30,9 @@ echo "  >> Add Execute Permission to $JAR_NAME"
 
 chmod +x $JAR_NAME
 
-echo "  >> $JAR_NAME Execute \n"
+echo "  >> $JAR_NAME Execute"
+
+echo "  >> deploy END"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
