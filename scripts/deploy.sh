421c4459db00e3952bc35d39f3dd57d4a97ba8b3
이용상 <gaonoiys@gmail.com>
2023-05-11 01:31:11 +0900

    1.0.1 version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,7 +32,9 @@ chmod +x $JAR_NAME
 
 echo "  >> $JAR_NAME Execute"
 
-echo "  >> deploy END"
+echo "  >> Deploy Proccess END ------------------------------------------------------------------------------------------------------"
+
+echo " "
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
